88695f70ab1469d324cb68aad18953caf792b1ae
Maxime <maxime.m62@gmail.com>
2017-06-21 19:12:39 +0200

    Initial Commit

---
+++ Example/Pods/Target Support Files/HydraKit/HydraKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HydraKit : NSObject
+@end
+@implementation PodsDummy_HydraKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-HydraKit_Tests/Pods-HydraKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HydraKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HydraKit_Tests
+@end
