f76bd40537d88fe823a2c35eea038063fc55cdf8
Maxime <maxime.m62@gmail.com>
2017-06-25 18:55:03 +0200

    Add test on getId

--- Example/Tests/HydraTests.swift
+++ Example/Tests/HydraTests.swift
@@ -47,4 +47,25 @@ class HydraTests: XCTestCase {
         XCTAssertEqual(session.lastURL?.absoluteString, "http://url.test/api/hydraObjs?name=myTest&note=3&extra=my%20extra%20arg")
         XCTAssertTrue(dataTask.resumeCall)
     }
+
+    func testHydraGetIdSuccess() {
+        let session = MockURLSession()
+        let dataTask = Task()
+        session.nextDataTask = dataTask
+        session.nextData = "{\"@id\":\"/api/hydraObjs/7508\",\"@type\":\"hydraObjs\"}".data(using: .utf8)
+        let hydra: Hydra = Hydra(endpoint: "http://url.test", urlSession: session)
+
+        hydra.get(HydraObj.self, id: 7508) { results in
+            switch results {
+            case .success(let results):
+                XCTAssertEqual(results.hydraId, "/api/hydraObjs/7508")
+            case .failure(_):
+                //never call
+                XCTAssertTrue(false)
+            }
+        }
+
+        XCTAssertEqual(session.lastURL?.absoluteString, "http://url.test/api/hydraObjs/7508")
+        XCTAssertTrue(dataTask.resumeCall)
+    }
 }
